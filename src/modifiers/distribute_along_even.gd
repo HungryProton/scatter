e57118b1de6bc682bef4e0fd0c026013f9bfd403
Addmix <addmixtaylor@gmail.com>
2021-01-11 16:52:50 -0700

    Fixed align to path and some placement shinanegans

--- src/modifiers/distribute_along_even.gd
+++ src/modifiers/distribute_along_even.gd
@@ -5,9 +5,10 @@ extends "base_modifier.gd"
 export(float) var interval = 1.0
 export(float, 0, 100) var offset = 0.0
 export(bool) var align_to_path = false
-export(int, "X", "Y", "Z") var align_axis = 0
-export(int, "X", "Y", "Z") var align_up_axis = 1
-
+export(int) var align_up_axis = 0
+export(bool) var restrict_x = false
+export(bool) var restrict_y = false
+export(bool) var restrict_z = false
 
 func _init() -> void:
 	display_name = "Distribute Along Path (Even)"
@@ -27,58 +28,44 @@ func _process_transforms(transforms, _seed) -> void:
 		return
 	
 	
-	
 	transforms.resize(total_count)
 	for i in transforms.list.size():
 		var data : Array = path.get_pos_and_normal(fmod(i * interval + abs(offset), length + fmod(length, interval)))
 		var pos: Vector3 = data[0]
 		var normal: Vector3 = data[1]
 		var t : Transform = transforms.list[i]
-
-		if align_to_path:
-			if align_axis == align_up_axis:
-				warning += """
-				The alignment axis is the same as the up axis.
-				No alignment has been done.
-				"""
-#			print(normal)
-			t = t.looking_at(normal, get_align_up_vector())
-			#after looking at
-			
 		
 		t.origin = pos
+		
+		if align_to_path:
+			normal.x *= int(!restrict_x)
+			normal.y *= int(!restrict_y)
+			normal.z *= int(!restrict_z)
+			
+			t = t.looking_at(normal + pos, get_align_up_vector(align_up_axis))
+			
+			
+		
 		transforms.list[i] = t
 
-
-
-func get_align_vector(t : Transform) -> Vector3:
-	match align_axis:
+static func get_align_up_vector(align : int) -> Vector3:
+	var axis : Vector3
+	match align:
 		#x
 		0:
-			return t.basis.y.normalized()
+			print(1)
+			axis = Vector3.RIGHT
 		#y
 		1:
-			return t.basis.y.normalized()
+			print(2)
+			axis = Vector3.UP
 		#z
 		2:
-			return t.basis.z.normalized()
-		_:
-			#default return x axis
-			return t.basis.x.normalized()
-
-func get_align_up_vector() -> Vector3:
-	match align_up_axis:
-		#x
-		0:
-			return Vector3.RIGHT
-		#y
-		1:
-			return Vector3.UP
-		#z
-		2:
-			return Vector3.BACK
+			print(3)
+			axis = Vector3.BACK
 		_:
 			#default return y axis
-			return Vector3.UP
+			print(4)
+			axis = Vector3.UP
 	
-	
+	return axis
