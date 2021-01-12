6de2b63d8db113271caa856161d988d227786fba
HungryProton <exb2800+github@protonmail.com>
2021-01-12 21:29:31 +0100

    jumped version number to 2.2.0, minor fixes du to previous PR

--- src/modifiers/distribute_along_even.gd
+++ src/modifiers/distribute_along_even.gd
@@ -5,7 +5,7 @@ extends "base_modifier.gd"
 export(float) var interval = 1.0
 export(float) var offset = 0.0
 export(bool) var align_to_path = false
-export(int) var align_up_axis = 1
+export(int, "X", "Y", "Z") var align_up_axis = 1
 export(bool) var restrict_x = false
 export(bool) var restrict_y = false
 export(bool) var restrict_z = false
@@ -19,33 +19,33 @@ func _process_transforms(transforms, _seed) -> void:
 	var path = transforms.path
 	var length: float = path.curve.get_baked_length()
 	var total_count: int = round(length / interval)
-	
-	
+
+
 	if total_count == 0:
 		warning += """
 		The interval is larger than the curve length.
 		No transforms could be placed."""
 		return
-	
-	
+
+
 	transforms.resize(total_count)
 	for i in transforms.list.size():
 		var data : Array = path.get_pos_and_normal(fmod(i * interval + abs(offset), length - fmod(length, interval)))
 		var pos: Vector3 = data[0]
 		var normal: Vector3 = data[1]
 		var t : Transform = transforms.list[i]
-		
+
 		t.origin = pos
-		
+
 		if align_to_path:
 			#axis restrictions
 			normal.x *= int(!restrict_x)
 			normal.y *= int(!restrict_y)
 			normal.z *= int(!restrict_z)
 			#this does not like restricting both x and z simulatneously
-			
+
 			t = t.looking_at(normal + pos, get_align_up_vector(align_up_axis))
-		
+
 		transforms.list[i] = t
 
 static func get_align_up_vector(align : int) -> Vector3:
@@ -63,5 +63,5 @@ static func get_align_up_vector(align : int) -> Vector3:
 		_:
 			#default return y axis
 			axis = Vector3.UP
-	
+
 	return axis
