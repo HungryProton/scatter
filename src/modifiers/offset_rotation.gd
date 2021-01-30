313e55e25a79ac1d1fbd7b4f02c9e3f97c8fa8c8
HungryProton <exb2800+github@protonmail.com>
2021-01-30 16:51:25 +0100

    incorrect unit used in offset rotation

--- src/modifiers/offset_rotation.gd
+++ src/modifiers/offset_rotation.gd
@@ -12,12 +12,17 @@ func _init() -> void:
 
 
 func _process_transforms(transforms, _global_seed : int) -> void:
+	var rotation_rad := Vector3.ZERO
+	rotation_rad.x = deg2rad(rotation.x)
+	rotation_rad.y = deg2rad(rotation.y)
+	rotation_rad.z = deg2rad(rotation.z)
+
 	var basis: Basis
 	for t in transforms.list.size():
 		basis = transforms.list[t].basis
 
-		basis = basis.rotated(float(local_space) * basis.x + float(!local_space) * Vector3(1, 0, 0), rotation.x)
-		basis = basis.rotated(float(local_space) * basis.y + float(!local_space) * Vector3(0, 1, 0), rotation.y)
-		basis = basis.rotated(float(local_space) * basis.z + float(!local_space) * Vector3(0, 0, 1), rotation.z)
+		basis = basis.rotated(float(local_space) * basis.x + float(!local_space) * Vector3(1, 0, 0), rotation_rad.x)
+		basis = basis.rotated(float(local_space) * basis.y + float(!local_space) * Vector3(0, 1, 0), rotation_rad.y)
+		basis = basis.rotated(float(local_space) * basis.z + float(!local_space) * Vector3(0, 0, 1), rotation_rad.z)
 
 		transforms.list[t].basis = basis
