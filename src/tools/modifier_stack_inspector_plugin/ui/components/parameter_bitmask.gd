dc2d3f786df861e4cb7094a73f0c2a9ee4e91231
HungryProton <hungryproton@pm.me>
2021-05-22 13:09:47 +0200

    add: popup with layer names in the bitmask component

--- src/tools/modifier_stack_inspector_plugin/ui/components/parameter_bitmask.gd
+++ src/tools/modifier_stack_inspector_plugin/ui/components/parameter_bitmask.gd
@@ -4,8 +4,10 @@ extends "base_parameter.gd"
 
 onready var _label: Label = $Label
 onready var _root: Control = $MarginContainer/HBoxContainer/GridContainer
+onready var _menu_button: MenuButton = $MarginContainer/HBoxContainer/MenuButton
 
 var _buttons: Array
+var _popup: PopupMenu
 
 
 func _ready() -> void:
@@ -14,17 +16,38 @@ func _ready() -> void:
 			_buttons.push_front(c)
 			c.connect("pressed", self, "_on_button_pressed")
 
+	_popup = _menu_button.get_popup()
+	var layer_name := ""
+	for i in 20:
+		if i != 0 and i % 5 == 0:
+			_popup.add_separator("", 100 + i)
+
+		layer_name = ProjectSettings.get_setting("layer_names/3d_physics/layer_" + String(i + 1))
+		if layer_name.empty():
+			layer_name = "Layer " + String(i + 1)
+		_popup.add_check_item(layer_name, 19 - i)
+
+	_sync_popup_state()
+	_popup.connect("id_pressed", self, "_on_id_pressed")
+
 
 func set_parameter_name(text: String) -> void:
 	_label.text = text
 
 
 func _set_value(val: String) -> void:
-	var binary_string: String = dec2bin(int(val))
+	var binary_string: String = _dec2bin(int(val))
 	var length = binary_string.length()
-	binary_string = binary_string.substr(length - 20, length)
+
+	if length < 20:
+		binary_string = binary_string.pad_zeros(20)
+	elif length > 20:
+		binary_string = binary_string.substr(length - 20, length)
+
 	for i in 20:
 		_buttons[i].pressed = binary_string[i] == "1"
+
+	_sync_popup_state()
 
 
 func get_value() -> String:
@@ -32,22 +55,23 @@ func get_value() -> String:
 	for b in _buttons:
 		binary_string += "1" if b.pressed else "0"
 
-	var val = bin2dec(binary_string)
+	var val = _bin2dec(binary_string)
 	return String(val)
 
 
-func dec2bin(var value: int) -> String:
+func _dec2bin(var value: int) -> String:
 	var binary_string = ""
 
 	while value != 0:
 		var m = value % 2
 		binary_string = String(m) + binary_string
+		# warning-ignore:integer_division
 		value = value / 2
 
 	return binary_string
 
 
-func bin2dec(var binary_string: String) -> int:
+func _bin2dec(var binary_string: String) -> int:
 	var decimal_value = 0
 	var count = binary_string.length() - 1
 
@@ -58,5 +82,23 @@ func bin2dec(var binary_string: String) -> int:
 	return decimal_value
 
 
+func _sync_popup_state() -> void:
+	if not _popup:
+		return
+
+	for i in 20:
+		var idx = _popup.get_item_index(i)
+		_popup.set_item_checked(idx, _buttons[i].pressed)
+
+
 func _on_button_pressed() -> void:
-	_on_value_changed(get_value())
+	_on_value_changed(null)
+	_sync_popup_state()
+
+
+func _on_id_pressed(id: int) -> void:
+	var idx = _popup.get_item_index(id)
+	var checked = not _popup.is_item_checked(idx)
+	_buttons[id].pressed = checked
+	_popup.set_item_checked(idx, checked)
+	_on_button_pressed()
