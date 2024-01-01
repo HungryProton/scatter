6821b63a120f4c003da7e4d6f9ec28658e8337dc
HungryProton <hungryproton@pm.me>
2024-01-01 11:31:10 +0100

    fixes #176 - node selector issues on 4.2 and with nodes without icon

--- addons/proton_scatter/src/stack/inspector_plugin/ui/modifier/components/parameter_node_selector.gd
+++ addons/proton_scatter/src/stack/inspector_plugin/ui/modifier/components/parameter_node_selector.gd
@@ -4,7 +4,6 @@ extends "base_parameter.gd"
 
 @onready var _label: Label = $%Label
 @onready var _select_button: Button = $%SelectButton
-@onready var _clear_button: Button = $%ClearButton
 @onready var _popup: ConfirmationDialog = $%ConfirmationDialog
 @onready var _tree: Tree = $%Tree
 
@@ -48,26 +47,39 @@ func get_value() -> NodePath:
 func _populate_tree() -> void:
 	_tree.clear()
 	var scene_root: Node = get_tree().get_edited_scene_root()
+	var editor_theme: Theme
 
-	var tmp = EditorPlugin.new() # TODO: check if this works in release builds
-	var gui: Control = tmp.get_editor_interface().get_base_control()
-	var editor_theme = gui.get_theme()
-	tmp.queue_free()
+	if EditorInterface.has_method("get_editor_theme"):
+		editor_theme = EditorInterface.get_editor_theme()
+	else:
+		# 4.1 backward compatibility
+		var tmp = EditorPlugin.new() # TODO: check if this works in release builds
+		var gui: Control = tmp.get_editor_interface().get_base_control()
+		editor_theme = gui.get_theme()
+		tmp.queue_free()
 
 	_create_items_recursive(scene_root, null, editor_theme)
 
 
-func _create_items_recursive(node, parent, theme) -> void:
+func _create_items_recursive(node: Node, parent: TreeItem, editor_theme: Theme) -> void:
 	if parent and not node.owner:
 		return # Hidden node.
 
 	var node_item = _tree.create_item(parent)
 	node_item.set_text(0, node.get_name())
 	node_item.set_meta("node", node)
-	node_item.set_icon(0, theme.get_icon(node.get_class(), "EditorIcons"))
+
+	var node_icon: Texture2D
+	var node_class := node.get_class()
+	if is_instance_valid(editor_theme):
+		if editor_theme.has_icon(node_class, "EditorIcons"):
+			node_icon = editor_theme.get_icon(node_class, "EditorIcons")
+		else:
+			node_icon = editor_theme.get_icon("Node", "EditorIcons")
+	node_item.set_icon(0, node_icon)
 
 	for child in node.get_children():
-		_create_items_recursive(child, node_item, theme)
+		_create_items_recursive(child, node_item, editor_theme)
 
 
 func _on_select_button_pressed() -> void:
